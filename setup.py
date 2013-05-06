83558d1e764f85db29886f4f9ff6369bcfe43872
Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
2013-05-06 13:54:30 -0300

    setup.py: version bump v1.1.1
    Signed-off-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
     name='pdf-link-checker',
-    version='1.1.0',
+    version='1.1.1',
     description='Reports broken hyperlinks in PDF documents',
     long_description=open('README.rst').read(),
 
